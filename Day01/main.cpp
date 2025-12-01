500ad507c67770a078b71a91a44be0e5ce7d9864
Obidkhon Akhmadkhonov <obbteam12@gmail.com>
2025-12-01 15:25:03 +0100

    added elapsed time for day 01

--- Day01/main.cpp
+++ Day01/main.cpp
@@ -1,17 +1,16 @@
 #include "../common/Utils.h"
+#include <chrono>
 
 int main() {
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     auto lines = AoC::loadInput("input.txt");
 
-    std::cout << "Read " << lines.size() << " lines from input." << std::endl;
 
     int clock = 50, ans = 0;
     for (std::string &line: lines) {
-        std::cout << "Input " << clock << std::endl;
 
         if (line[0] == 'L') clock -= std::stoi(line.substr(1));
         else if (line[0] == 'R') clock += std::stoi(line.substr(1));
-        std::cout << clock << " : " << ans << std::endl;
 
         ans += std::abs(clock / 100);
         clock %= 100;
@@ -20,9 +19,12 @@ int main() {
             ans++;
             clock = 100 - std::abs(clock);
         }
-        std::cout << "Output " <<  clock << " : " << ans << std::endl;
     }
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
     std::cout << "Ans: " << ans << std::endl;
+    std::cout << "Run time = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() <<
+            "[ms]" << std::endl;
 
     return 0;
 }
