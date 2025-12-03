75c9437a20a4cdefde8da511165065c827b92160
Obidkhon Akhmadkhonov <obbteam12@gmail.com>
2025-12-03 10:38:42 +0100

    Day04 folder added

---
+++ Day04/part1/part1.h
@@ -0,0 +1,18 @@
+//
+// Created by Obidkhon Akhmadkhonov on 02/12/2025.
+//
+
+#ifndef ADVENT_OF_CODE_2025_PART1_H
+#define ADVENT_OF_CODE_2025_PART1_H
+#include <vector>
+
+
+class Part1 {
+public:
+    void solve();
+private:
+    long long parseAndSolve(std::vector<std::string> & lines);
+};
+
+
+#endif //ADVENT_OF_CODE_2025_PART1_H
