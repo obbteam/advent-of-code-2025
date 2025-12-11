5d7c750d34e991b66132917de01e470889f9e4c1
Obidkhon Akhmadkhonov <obbteam12@gmail.com>
2025-12-11 17:01:27 +0100

    Day12 folder created

---
+++ Day12/part2/part2.h
@@ -0,0 +1,19 @@
+//
+// Created by Obidkhon Akhmadkhonov on 02/12/2025.
+//
+
+#ifndef ADVENT_OF_CODE_2025_PART2_H
+#define ADVENT_OF_CODE_2025_PART2_H
+#include <vector>
+
+
+class Part2 {
+public:
+    void solve();
+
+private:
+    static long long parseAndSolve(std::vector<std::string> &lines);
+};
+
+
+#endif //ADVENT_OF_CODE_2025_PART2_H
