a0019208742a286ed8f549af6bfeb14ad6d4d19a
Obidkhon Akhmadkhonov <obbteam12@gmail.com>
2025-12-11 14:38:07 +0100

    Day11 folder created

---
+++ Day11/part1/part1.cpp
@@ -0,0 +1,21 @@
+#include "part1.h"
+
+#include "../common/Utils.h"
+
+
+void Part1::solve() {
+    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+
+    auto lines = AoC::loadInput("input.txt");
+    const long long result = parseAndSolve(lines);
+
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    printMetaData(Record(2025, 1, 1, result, start, end));
+}
+
+long long Part1::parseAndSolve(std::vector<std::string> &lines) {
+    long long result = 0;
+
+    return result;
+}
---
+++ Day11/part2/part2.cpp
@@ -0,0 +1,21 @@
+#include "part2.h"
+
+#include "../common/Utils.h"
+
+
+void Part2::solve() {
+    std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+
+    auto lines = AoC::loadInput("input.txt");
+    const long long result = parseAndSolve(lines);
+
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+
+    printMetaData(Record(2025, 1, 2, result, start, end));
+}
+
+long long Part2::parseAndSolve(std::vector<std::string> &lines) {
+    long long result = 0;
+
+    return result;
+}
